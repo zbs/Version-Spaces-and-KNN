a204be6e9ffb13dccef8a96df9551c06d2a255ba
Zach Silversmith <cyberm4tic@gmail.com>
2012-09-03 14:07:19 -0400

    Initial setup of skeleton code

---
+++ SongRecommendation/Main.py
@@ -0,0 +1,14 @@
+'''
+Created on Sep 3, 2012
+
+@author: zach
+'''
+def run_knn(k, weighted, similarity_metric, user, artist=None):
+    all_users = get_users()
+    sorted_users_by_similarity = sort_users_by_similarity(user, all_users, similarity_metric)
+    top_ten_users = sorted_users_by_similarity[:10]
+    ranking_vector = calculate_ranking_vector(user, top_ten_users, weighted)
+    return get_precision_at_ten(ranking_vector)
+
+if __name__ == '__main__':
+    pass
