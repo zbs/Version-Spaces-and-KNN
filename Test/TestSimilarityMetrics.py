9a17de2f439b52ba1cc8b954acbc87b6ae74d958
Zach Silversmith <cyberm4tic@gmail.com>
2012-09-04 19:23:26 -0400

    Adding the test framework

---
+++ Test/TestSimilarityMetrics.py
@@ -0,0 +1,26 @@
+'''
+Created on Sep 4, 2012
+
+@author: zach
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        # type self.assert, then hit Ctrl + Space
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
