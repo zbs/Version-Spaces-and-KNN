2dcf77f33d4b4f7fdc27279d339247195a73d392
Zach Silversmith <cyberm4tic@gmail.com>
2012-09-05 01:18:35 -0400

    Allows a different way to initialize a user

--- SongRecommendation/User.py
+++ SongRecommendation/User.py
@@ -4,14 +4,17 @@ Created on Sep 3, 2012
 @author: zach
 '''
 class User():
-    def __init__(self, user_line):
-        self.id, songs = user_line.split(' - ') 
-        self.id = int(self.id)
-        def f(x):
-            y = x.split(':')
-            y[1] = int(y[1])
-            return tuple(y)
-        self.songs = dict(map(f, songs.split(' ')))
+    def __init__(self, user_line, user_id=None, user_songs=None):
+        if user_id and user_songs:
+            self.id, self.songs = user_id, user_songs
+        else:    
+            self.id, songs = user_line.split(' - ') 
+            self.id = int(self.id)
+            def f(x):
+                y = x.split(':')
+                y[1] = int(y[1])
+                return tuple(y)
+            self.songs = dict(map(f, songs.split(' ')))
     
     def get_similarity_funct(self, similarity_metric):
         return lambda other_user: similarity_metric(self.songs, other_user.songs)
