5d491f2d6ea76ee7a97cb0d3b0e3d50cc1c8a4f1
Sercan Mihmanli <msm267@cornell.edu>
2012-09-04 21:20:04 -0400

    ddded similarity metrics and unit tests

--- SongRecommendation/Main.py
+++ SongRecommendation/Main.py
@@ -31,9 +31,53 @@ def get_precision_at_ten(ranking_vector):
 def run_knn(k, weighted, similarity_metric, user, artist=None):
     all_users = get_users()
     sorted_users_by_similarity = sort_users_by_similarity(user, all_users, similarity_metric)
-    top_ten_users = sorted_users_by_similarity[:10]
-    ranking_vector = calculate_ranking_vector(user, top_ten_users, weighted)
+    top_k_users = sorted_users_by_similarity[:10]
+    ranking_vector = calculate_ranking_vector(user, top_k_users, weighted)
     return get_precision_at_ten(ranking_vector)
+
+def euclidean_distance(user1_songs, user2_songs):
+    
+    total = 0
+    user_dict = dict([])
+    
+    for song in user1_songs:
+        user_dict[song] = user1_songs[song]
+        
+    for song in user2_songs:
+        if song in user_dict:
+            user_dict[song] = user_dict[song] - user2_songs[song]
+        else:
+            user_dict[song] = user2_songs[song]
+            
+    for song in user_dict:
+        total += user_dict[song]**2
+                
+            
+        
+        
+    return 1. / total**(1./2.)
+
+def dot_product(user1_songs, user2_songs):
+    song_intersection = set.intersection(set(user1_songs.keys()), set(user2_songs.keys()))
+    product = 0
+    for song in song_intersection:
+        product += user1_songs[song] * user2_songs[song]
+        
+    return product
+
+def cos_distance(user1_songs, user2_songs):
+    return dot_product(user1_songs, user2_songs) / (magnitude(user1_songs) * magnitude(user2_songs))
+    #return (magnitude(user1_songs) * magnitude(user2_songs))
+    
+def magnitude(vector):
+    return sum(map(lambda x: x**2, vector.values())) ** (1./2.)
+     
+            
+            
+            
+            
+              
+    
 
 if __name__ == '__main__':
     pass
